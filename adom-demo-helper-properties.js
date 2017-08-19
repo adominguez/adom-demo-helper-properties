0980b22d0b24fe54c455d0f054bdf1ebfd2bf3bb
adominguez <dmartin.alberto@gmail.com>
2017-08-19 17:44:48 +0200

    feat(html, scss, js, demo/index, README): add functionality css var

--- adom-demo-helper-properties.js
+++ adom-demo-helper-properties.js
@@ -13,12 +13,26 @@
         type: String,
         observer: '_setHeading'
       },
+      _file: {
+        type: String,
+        observer: '_setFile'
+      },
+      _scss: {
+        type: String,
+        observer: '_setScss'
+      },
+      _mixins: {
+        type: Array,
+        value: function () {
+          return [];
+        }
+      },
       /**
        * set the events fired
        */
       toastEvents: {
         type: Array,
-        value: function() {
+        value: function () {
           return [];
         },
         notify: true,
@@ -26,14 +40,14 @@
 
       propertiesSetted: {
         type: Array,
-        value: function() {
+        value: function () {
           return [];
         },
         notify: true
       },
       _propertiesBinded: {
         type: Array,
-        value: function() {
+        value: function () {
           return [];
         },
         notify: true,
@@ -41,45 +55,108 @@
       }
     },
 
-    attached: function() {
-      for(var i = 0; i < this.toastEvents.length; i++) {
-        this.addEventListener(this.toastEvents[i], function(e) {
-          this._showToast(e)
+    ready: function () {
+      for (var i = 0; i < this.toastEvents.length; i++) {
+        this.addEventListener(this.toastEvents[i], function (e) {
+          this._showToast(e);
+          if(e.detail.value) {
+            this.$.toastContent.innerHTML=e.detail.value
+          }
         })
       }
-      this._propertiesBinded = this.propertiesSetted;
     },
 
-    _setHeading: function() {
+
+    _setHeading: function (componentName) {
       this.$$('#heading').innerHTML = this.children[0].getAttribute('data-heading');
       this.$$('#description').innerHTML = this.children[0].getAttribute('data-description');
+      this._file = '../bower_components/' + componentName + '/' + componentName + '-styles.html';
     },
-    _showToast: function(e) {
+    _setFile: function() {
+      var self = this;
+      /**
+       * Call for get the scss file of component
+       */
+      var request = new XMLHttpRequest();
+      request.open('GET', self._file, true);
+      request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+          // Save the scss in a property
+          self._scss = request.responseText;
+        } else {
+          self._propertiesBinded = self.propertiesSetted;
+          console.error('file not found');
+        }
+      };
+      request.onerror = function () {
+        ajaxErr(request);
+      };
+      request.send();
+    },
+    _showToast: function (e) {
       this.$$('#toast').text = e.type;
       this.$$('#toast').open();
     },
-    _setProperties: function() {
+    _setProperties: function () {
       var newProperties = "";
-      var html="";
-      var snippet="";
-      for(var i = 0; i < this._propertiesBinded.length; i++) {
+      var html = "";
+      var snippet = "";
+      var style = "";
 
-        if((this._propertiesBinded[i].list) && (this._propertiesBinded[i].list[this._propertiesBinded[i].selected].value !== "false")) {
-          if(this._propertiesBinded[i].list[this._propertiesBinded[i].selected].value === "true") {
+      if (this._mixins.length > 0) {
+        style += '<style is="custom-style">' + this.componentName + '{'
+        for (var j = 0; j < this._mixins.length; j++) {
+          style += this._mixins[j].mixin + ':' + this._mixins[j].value + ';'
+        }
+        style += '}</style>'
+      }
+
+      for (var i = 0; i < this._propertiesBinded.length; i++) {
+
+        if ((this._propertiesBinded[i].list) && (this._propertiesBinded[i].list[this._propertiesBinded[i].selected].value !== "false")) {
+          if (this._propertiesBinded[i].list[this._propertiesBinded[i].selected].value === "true") {
             newProperties += ' ' + this._propertiesBinded[i].label;
           } else {
-            newProperties += this._propertiesBinded[i].label + '="' + this._propertiesBinded[i].list[this._propertiesBinded[i].selected].value +'" ';
+            newProperties += this._propertiesBinded[i].label + '="' + this._propertiesBinded[i].list[this._propertiesBinded[i].selected].value + '" ';
           }
         } else {
-          if(!this._propertiesBinded[i].list) {
-            newProperties += this._propertiesBinded[i].label + '="' + this._propertiesBinded[i].value +'" '
+          if (!this._propertiesBinded[i].list) {
+            newProperties += this._propertiesBinded[i].label + '="' + this._propertiesBinded[i].value + '" '
           }
         }
       }
-      html = '<template is="dom-bind"><'+ this.componentName + ' ' + newProperties + '></'+ this.componentName + '></template>'
+      html = '<template is="dom-bind">'+ style +'<'+ this.componentName + ' ' + newProperties + '></'+ this.componentName + '></template>'
       snippet = '<'+ this.componentName + ' ' + newProperties + '></'+ this.componentName + '>'
       this.children[0].innerHTML = html;
       this.children[0]._markdown = '```' + snippet + '```';
+    },
+    _setScss: function (_scss) {
+      var mixin = [];
+      var array = []
+      // Find the first word
+      var reg1 = /([^\s]+)/;
+      // Find the second word
+      var reg2 = /\s+([^\s]+)/;
+      for (var i = 0; i < _scss.split('var(').length; i++) {
+        if (i > 0) {
+          if(array.indexOf(_scss.split('var(')[i].match(reg1)[0].slice(0, -1)) === -1) {
+            mixin.push({
+              "mixin": _scss.split('var(')[i].match(reg1)[0].slice(0, -1),
+              "value": _scss.split('var(')[i].match(reg2)[0].slice(1, -2)
+            });
+            array.push(_scss.split('var(')[i].match(reg1)[0].slice(0, -1))
+          }
+        }
+      }
+      this._mixins = mixin;
+      this._propertiesBinded = this.propertiesSetted;
+    },
+
+    _computeColor: function(item) {
+      if(item.mixin.slice(-5) === 'color') {
+        return true;
+      }
+      return false;
     }
 
   });
